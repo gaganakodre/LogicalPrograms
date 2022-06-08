4207b141186a93255e2e3572ba90265f85940247
shreegowri <gaganandh@gmail.com>
2022-06-08 14:07:55 +0530

    [shreegowri] Fibonnaci

--- CLogicaProgramsl/CLogicaProgramsl/Program.cs
+++ CLogicaProgramsl/CLogicaProgramsl/Program.cs
@@ -6,12 +6,17 @@
         {
             Console.WriteLine("Enter the realtive number to perform some operation");
             Console.WriteLine("Press 1 to perform the perfect number");
+            Console.WriteLine("Press 4 to perform the Fibonacci serioes");
             int problem = Convert.ToInt32(Console.ReadLine());
             switch (problem)
             {
                 case 1:
                     Perfectnumber1 Pnum = new Perfectnumber1();
                     Pnum.Perfectnum();
+                    break;
+                case 4:
+                    Fibonaci1 fib = new Fibonaci1();
+                    fib.fibonacci();
                     break;
             }
         }
