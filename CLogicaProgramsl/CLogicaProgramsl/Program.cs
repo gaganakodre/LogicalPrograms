4963be629133dbca9ba05e61acdb94a8d6daf4e6
shreegowri <gaganandh@gmail.com>
2022-06-08 13:55:43 +0530

    [shreegowri]Perfect NUmber

---
+++ CLogicaProgramsl/CLogicaProgramsl/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CLogicaProgramsl
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the realtive number to perform some operation");
+            Console.WriteLine("Press 1 to perform the perfect number");
+            int problem = Convert.ToInt32(Console.ReadLine());
+            switch (problem)
+            {
+                case 1:
+                    Perfectnumber1 Pnum = new Perfectnumber1();
+                    Pnum.Perfectnum();
+                    break;
+            }
+        }
+    }
+}
