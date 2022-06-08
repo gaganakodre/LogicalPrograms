e739735360262034e05b4ad2802f196d192d19d0
shreegowri <gaganandh@gmail.com>
2022-06-08 14:12:51 +0530

    [shreegowri] stopwatch

--- CLogicaProgramsl/CLogicaProgramsl/Program.cs
+++ CLogicaProgramsl/CLogicaProgramsl/Program.cs
@@ -6,6 +6,8 @@
         {
             Console.WriteLine("Enter the realtive number to perform some operation");
             Console.WriteLine("Press 1 to perform the perfect number");
+            Console.WriteLine("Press 2 to perform the  stopwatch program");
+            Console.WriteLine("Press 3 to perform the  revesering a number");
             Console.WriteLine("Press 4 to perform the Fibonacci serioes");
             int problem = Convert.ToInt32(Console.ReadLine());
             switch (problem)
@@ -13,6 +15,14 @@
                 case 1:
                     Perfectnumber1 Pnum = new Perfectnumber1();
                     Pnum.Perfectnum();
+                    break;
+                case 2:
+                    StopWatch SWatch = new StopWatch();
+                    SWatch.StopWatchPrg();
+                    break;
+                case 3:
+                    ResverseNumber RevNum = new ResverseNumber();
+                    RevNum.ReverseNum();
                     break;
                 case 4:
                     Fibonaci1 fib = new Fibonaci1();
---
+++ CLogicaProgramsl/CLogicaProgramsl/StopWatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLogicaProgramsl
+{
+    internal class StopWatch
+    {
+        public void StopWatchPrg()
+        {
+            Console.WriteLine("press Enter to start");
+            Console.ReadLine();
+            DateTime Start = DateTime.Now;
+
+            Console.WriteLine("press Enter to stop.");
+            Console.ReadLine();
+            DateTime Stop = DateTime.Now;
+
+            Console.WriteLine("Elapsed Time =" + (Stop - Start));
+        }
+    }
+}
